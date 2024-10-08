df3ad8374303fb1fcce66426e5ff6a187fb3c91e
Gourishankar <121673122+3211SHANKAR@users.noreply.github.com>
2024-10-08 17:36:13 +0530

    Time: 16 ms (23.81%), Space: 20.4 MB (95.3%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -4,15 +4,10 @@ public:
         int n=nums.size();
         int cp=0;
         int cn=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                cp++;
-            }
-            else if(nums[i]<0){
-                cn++;
-            }
+        for(auto a:nums){
+            if(a>0)cp++;
+            else if(a<0)cn++;
         }
-        if(cp>=cn)return cp;
-        return cn;
+        return max(cp,cn);
     }
 };
