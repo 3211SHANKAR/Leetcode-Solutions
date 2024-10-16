c060a3750ebcd697ac0a5bd50f799f33a6817c3f
Gourishankar <121673122+3211SHANKAR@users.noreply.github.com>
2024-10-16 16:25:18 +0530

    Time: 2 ms (55.7%), Space: 7.9 MB (7.55%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        for(int i=2;i<=n-2;i++){
+            string s = "";
+            while(n){
+                s+=to_string(n%i);
+                n/=i;
+            }
+            int n=s.length();
+            int k=0;
+            int j=n;
+            while(k<j){
+                if(s[k]!=s[j])return false;
+                k++;
+                j--;
+            }
+        }
+        return true;
+    }
+};
