f359613903fe5215b1acb2eebeaecf124eccc8b3
Gourishankar <121673122+3211SHANKAR@users.noreply.github.com>
2025-02-06 15:34:46 +0530

    Time: 800 ms (9.4%), Space: 99.2 MB (15.67%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -6,15 +6,19 @@ public:
         int prod=0;
         for(int i=0;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
-                mp[prod=nums[i]*nums[j]]++;
+                prod=nums[i]*nums[j];
+                mp[prod]++;
             }
         }
-       int ans=0; 
+      
+       int ans=0;
+        
         for(auto &a:mp){
             int id= a.second;
         if(id>1){
             ans+= 8*((id)*(id-1))/2;
         }
+
        }
         return ans;
     }
