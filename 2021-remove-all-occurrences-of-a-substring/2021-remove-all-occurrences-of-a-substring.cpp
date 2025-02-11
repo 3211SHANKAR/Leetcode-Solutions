806908131095eb99045fbd85c29ce624c7f90f5f
Gourishankar <121673122+3211SHANKAR@users.noreply.github.com>
2025-02-11 19:51:50 +0530

    Time: 57 ms (5.85%), Space: 21.8 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -17,17 +17,58 @@
 // };
 
 
+// class Solution {
+// public:
+//     string removeOccurrences(string s, string part) {
+//         while (s.find(part) != string::npos) {  
+//             s.erase(s.find(part), part.length());
+//         }
+//         return s;
+//     }
+// };
+
+
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while (s.find(part) != string::npos) {  
-            s.erase(s.find(part), part.length());
+        stack<char> st;
+        int t = part.length();
+      
+        for (char c : s) {
+            st.push(c);  
+            
+            if (st.size() >= t) {  
+
+                string temp = "";
+                vector<char> popped_chars; 
+                
+                bool match = true;
+                for (int i = t - 1; i >= 0; i--) {
+                    if (st.top() != part[i]) {
+                        match = false;
+                    }
+                    popped_chars.push_back(st.top());
+                    st.pop();
+                }
+
+            
+                if (!match) {
+                    for (int i = popped_chars.size() - 1; i >= 0; i--) {
+                        st.push(popped_chars[i]);
+                    }
+                }
+            }
         }
-        return s;
+
+  
+        string ans = "";
+        while (!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
+        }
+
+        return ans;
     }
 };
-
-
-
 
 
