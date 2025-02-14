90bfeb7b6417ea98c0d6912f0de5613046480dfd
Gourishankar <121673122+3211SHANKAR@users.noreply.github.com>
2025-02-15 00:26:58 +0530

    Time: 27 ms (43.54%), Space: 77.9 MB (93.67%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,7 +1,7 @@
 class ProductOfNumbers {
 public:
 
-  vector<int>v;
+  vector<int>v;// cummulative product store karenge
   int n;
     ProductOfNumbers() {
       v.clear();
@@ -9,16 +9,22 @@ public:
     }
     
     void add(int num) {
-        v.push_back(num);
+        if(num==0){
+            v={};
+            n=0;
+        }
+        else{
+            if(v.empty()){v.push_back(num);}
+            else { v.push_back(num*v[n-1]);}
+            n++;
+        }
     }
     
     int getProduct(int k) {
-        int prod=1;
-        int n=v.size();
-        for(int i=n-k;i<n;i++){
-            prod*=v[i];
-        }
-        return prod;
+       if(k==n)return v[n-1];
+       else if(k>n)return 0;
+       
+      return v[n-1]/v[n-k-1];
     }
 };
 
