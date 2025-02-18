f6ae85bbdb4a313f0121a10205586b4635fd8dd5
Gourishankar <121673122+3211SHANKAR@users.noreply.github.com>
2025-02-18 21:36:18 +0530

    Time: 0 ms (100%), Space: 8 MB (63.49%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,20 +1,41 @@
+// class Solution {
+// public:
+//     string smallestNumber(string pattern) {
+//         int count=1;
+//         stack<char>st;
+//         string nums="";
+//         int n=pattern.size();
+//         for(int i=0;i<=n;i++){
+//             st.push(count+'0');
+//             if(i==n|| pattern[i]=='I'){
+//                 while(!st.empty()){
+//                     nums+=st.top();
+//                     st.pop();
+//                 }
+//             }
+//              count++;
+//         }
+//         return nums;
+//     }
+// };
+
 class Solution {
 public:
     string smallestNumber(string pattern) {
-        int count=1;
-        stack<char>st;
-        string nums="";
-        int n=pattern.size();
-        for(int i=0;i<=n;i++){
-            st.push(count+'0');
-           
-            if(i==n|| pattern[i]=='I'){
-                while(!st.empty()){
-                    nums+=st.top();
-                    st.pop();
+        int count = 1;
+        vector<char> vec;  // Using vector instead of stack
+        string nums = "";
+        int n = pattern.size();
+        
+        for (int i = 0; i <= n; i++) {
+            vec.push_back(count + '0');
+            if (i == n || pattern[i] == 'I') {
+                while (!vec.empty()) {
+                    nums += vec.back();
+                    vec.pop_back();
                 }
             }
-             count++;
+            count++;
         }
         return nums;
     }
