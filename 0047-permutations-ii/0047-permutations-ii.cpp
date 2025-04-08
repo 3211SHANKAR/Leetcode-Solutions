8a86f7bf6614a6277dd504faa1a222cc408373b5
Gourishankar <121673122+3211SHANKAR@users.noreply.github.com>
2025-04-08 11:15:06 +0530

    Time: 7 ms (41.78%), Space: 15.6 MB (16.08%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -3,35 +3,30 @@ public:
     int n;
      vector<vector<int>>result;
 
-     void solve(unordered_map<int,int>&mp ,vector<int>&temp){
-
-        if(temp.size()==n){
-            result.push_back(temp);
-            return;
+     void solve(int idx ,vector<int>&nums){
+        if(idx==n){
+            result.push_back(nums);
+            return ;
         }
-        for(auto &[num,val]:mp){
-            if(val==0){
+        unordered_set<int>s;
+        for(int i=idx;i<n;i++){
+
+            
+            if(s.find(nums[i])!=s.end()){
                 continue;
             }
-
-            temp.push_back(num); //do 
-            mp[num]--;
-            solve(mp,temp); //explore
-
-            temp.pop_back(); //undo
-            mp[num]++;
-        
+            s.insert(nums[i]);
+            swap(nums[idx],nums[i]);
+            solve(idx+1,nums);
+            swap(nums[idx],nums[i]);
         }
+       
      }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto a:nums){
-            mp[a]++;
-        }
+        
         n=nums.size();
-        vector<int>temp;
-        solve(mp,temp);
+        solve(0,nums);
         return result;
     }
 };
